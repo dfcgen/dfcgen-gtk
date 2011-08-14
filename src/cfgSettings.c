ebb3a955b0a7cada444d5fbb3ef1d049fc6bc0a7
Ralf Hoppe <ralf.hoppe@ieee.org>
2011-08-14 15:48:35 +0000

    Update of file header (cvs2svn, copyright)

--- src/cfgSettings.c
+++ src/cfgSettings.c
@@ -6,19 +6,8 @@
  * \note     All double values written to the configuration file are formated
  *           in the "C" locale for LC_NUMERIC.
  *
- * \author   Copyright (c) 2006 Ralf Hoppe <ralf.hoppe@ieee.org>
+ * \author   Copyright (c) 2006, 2011 Ralf Hoppe <ralf.hoppe@ieee.org>
  * \version  $Header: /home/cvs/dfcgen-gtk/src/cfgSettings.c,v 1.3 2006-11-08 17:31:42 ralf Exp $
- *
- *
- * History:
- * $Log: not supported by cvs2svn $
- * Revision 1.2  2006/11/04 18:26:27  ralf
- * Further work (near 0.1 now)
- *
- * Revision 1.1.1.1  2006/09/11 15:52:19  ralf
- * Initial CVS import
- *
- *
  *
  ******************************************************************************/
 
