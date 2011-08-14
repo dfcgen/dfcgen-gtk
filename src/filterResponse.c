793bd2663262cb2371167639a45d4bc082b260aa
Ralf Hoppe <ralf.hoppe@ieee.org>
2011-08-14 15:55:57 +0000

    Update of file header (cvs2svn, copyright).

--- src/filterResponse.c
+++ src/filterResponse.c
@@ -6,17 +6,6 @@
  * \author   Copyright (c) Ralf Hoppe
  * \version  $Header: /home/cvs/dfcgen-gtk/src/filterResponse.c,v 1.3 2006-11-08 17:31:42 ralf Exp $
  *
- *
- * History:
- * $Log: not supported by cvs2svn $
- * Revision 1.2  2006/11/04 18:26:27  ralf
- * Further work (near 0.1 now)
- *
- * Revision 1.1.1.1  2006/09/11 15:52:19  ralf
- * Initial CVS import
- *
- *
- *
  ******************************************************************************/
 
 
