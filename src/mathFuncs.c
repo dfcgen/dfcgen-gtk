8f62b48c50db968f6473a7f97f5518ba93d6ab05
Ralf Hoppe <ralf.hoppe@ieee.org>
2011-08-14 15:57:19 +0000

    Update of file header (cvs2svn, copyright).

--- src/mathFuncs.c
+++ src/mathFuncs.c
@@ -3,16 +3,8 @@
  * \file
  *           Mathematical functions.
  *
- * \author   Copyright (c) 2006 Ralf Hoppe <ralf.hoppe@ieee.org>
+ * \author   Copyright (C) 2006, 2011 Ralf Hoppe <ralf.hoppe@ieee.org>
  * \version  $Header: /home/cvs/dfcgen-gtk/src/mathFuncs.c,v 1.2 2006-11-04 18:26:27 ralf Exp $
- *
- *
- * History:
- * $Log: not supported by cvs2svn $
- * Revision 1.1.1.1  2006/09/11 15:52:19  ralf
- * Initial CVS import
- *
- *
  *
  ******************************************************************************/
 
