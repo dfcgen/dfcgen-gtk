e7b286ce402c1f4f145e71ca3de5a52b1987a892
Ralf Hoppe <ralf.hoppe@ieee.org>
2011-08-14 15:55:12 +0000

    Update of file header (cvs2svn, copyright).

--- src/editDlg.c
+++ src/editDlg.c
@@ -3,16 +3,8 @@
  * \file
  *           \e Edit dialog(s).
  *
- * \author   Copyright (c) 2006 Ralf Hoppe <ralf.hoppe@ieee.org>
+ * \author   Copyright (c) 2006, 2011 Ralf Hoppe <ralf.hoppe@ieee.org>
  * \version  $Header: /home/cvs/dfcgen-gtk/src/editDlg.c,v 1.2 2006-11-04 18:26:27 ralf Exp $
- *
- *
- * History:
- * $Log: not supported by cvs2svn $
- * Revision 1.1.1.1  2006/09/11 15:52:19  ralf
- * Initial CVS import
- *
- *
  *
  ******************************************************************************/
 
