1dd55576719da74565cd915e78355b531071b91e
Ralf Hoppe <ralf.hoppe@ieee.org>
2011-08-14 15:58:59 +0000

    Update of file header (cvs2svn, copyright).

--- src/stdIirFilter.c
+++ src/stdIirFilter.c
@@ -3,19 +3,8 @@
  * \file
  *           Standard IIR filter coefficients generator.
  *
- * \author   Copyright (c) Ralf Hoppe
+ * \author   Copyright (C) 2006, 2011 Ralf Hoppe
  * \version  $Header: /home/cvs/dfcgen-gtk/src/stdIirFilter.c,v 1.3 2006-11-08 17:31:42 ralf Exp $
- *
- *
- * History:
- * $Log: not supported by cvs2svn $
- * Revision 1.2  2006/11/04 18:26:27  ralf
- * Further work (near 0.1 now)
- *
- * Revision 1.1.1.1  2006/09/11 15:52:19  ralf
- * Initial CVS import
- *
- *
  *
  ******************************************************************************/
 
