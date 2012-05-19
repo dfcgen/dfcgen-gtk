eaa33d126a7790f51827b945314e0afd0dbec61e
Ralf Hoppe <ralf.hoppe@ieee.org>
2012-05-19 16:02:58 +0000

    New option --enable-debug for configure script, inclusive CFLAGS.

--- include/doxygen.h
+++ include/doxygen.h
@@ -3,7 +3,7 @@
  * \file     doxygen.h
  *           Additional doxygen sections (no code).
  *
- * \author   Copyright (C) 2006, 2011, 2012 Ralf Hoppe <ralf.hoppe@ieee.org>
+ * \author   Copyright (C) 2006, 2011-2012 Ralf Hoppe <ralf.hoppe@ieee.org>
  * \version  $Id$
  *
  * \page        Configure
@@ -13,16 +13,14 @@
  *              - \c make \c [all] in root directory builds the package
  *              - \c make \c install in root directory installs the package
  * \section     Developers
- *              - Defining \c DEBUG gives more verbose error messages at the command
- *                line and enables additional checks
  *              - \c make \c dist in root directory generates a distribution package
  *              - \c make \c dist-debian in root directory builds a Debian package
+ *              - \c make \c distcheck in root directory checks the package for
+ *                distribution (and updates the \e gettext database and \e po files)
  *              - \c make \c update-po in \e po directory updates the
  *                strings list in file \e de.po
  *              - \c make \c all in \e po directory builds the \e mo file(s)
  *
- *
- * \todo        Correct the e-mail address
  *
  ******************************************************************************/
 
